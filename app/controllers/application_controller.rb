b8e2278d5e7a91165869f352cd4146f5cfa3a599
Hugo Le Morvan <lm.hugo@hotmail.fr>
2020-12-23 21:41:12 +0100

    ajout méta tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
+# app/controllers/application_controller.rb
 class ApplicationController < ActionController::Base
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
 end
